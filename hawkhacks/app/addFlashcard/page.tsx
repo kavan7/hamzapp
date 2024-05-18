ea1f3f0d62efaf44270a4cdcb7ad23579d147100
Enkai Liu <enkailiu@Enkais-Laptop.local>
2024-05-18 17:03:59 -0400

    hi

---
+++ hawkhacks/app/addFlashcard/page.tsx
@@ -0,0 +1,14 @@
+// pages/about.tsx
+
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the about page of our Next.js application.</p>
+    </div>
+  );
+};
+
+export default About;
--- hawkhacks/app/openai-test.tsx
+++ hawkhacks/app/openai-test.tsx
@@ -7,7 +7,7 @@ const openai = new OpenAI({
 async function main() {
   try {
     const completion = await openai.chat.completions.create({
-      messages: [{ role: "system", content: "You are a helpful assistant." }],
+      messages: [{ role: "system", content: "tell me a story  ." }],
       model: "gpt-3.5-turbo",
     });
 
