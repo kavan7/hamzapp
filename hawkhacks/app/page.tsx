9bffa3a9c1e76d2bf76a2ed6937743b3e0d1e23b
Hamza Ammar <68649005+HamzaAmmarPython@users.noreply.github.com>
2024-05-19 02:00:50 -0400

    made tribia

--- hawkhacks/app/page.tsx
+++ hawkhacks/app/page.tsx
@@ -5,6 +5,7 @@ import { PoseLandmarker, FilesetResolver, DrawingUtils, LandmarkData } from '@me
 import Flashcard from './components/Flashcard';
 import { calculateAngle } from './utils/MathUtils';
 import Link from 'next/link';
+import { AwesomeButton } from 'react-awesome-button';
 
 let leftStage = "up";
 let rightStage = "up";
@@ -12,12 +13,12 @@ let rightStage = "up";
 
 const initialQuestion = {
   question: "What is the capital of France?",
-  choices: ["Paris", "Berlin"] as [string, string],
+  choices: ["Berlin", "Paris"] as [string, string],
   correctAnswer: "Paris",
 };
 
 const getNewQuestion = async () => {
-  // Simulate an AI call to generate a new question
+  let quests: String[][] = [['Which language is primairly used for web development along with HTML and CSS', 'Javascript', 'Python', 'Javascript'], ['What is the purpose of a "for loop" in programming', 'To iterate over a sequence of elements', 'To define a new variable', 'To iterate over a sequence of elements'], ['Which of the following is a version control system', 'Git', 'SSH', 'Git'], ['What does the python function len() do', 'converts a value to a string', 'returns the length of an object', 'returns the length of an object'], ['What does SQL stand for', 'Structured Query Language', 'Simple Query List', 'Structured Query Language'], ['Which HTML tag is used to create a hyperlink', '<a>', '<link>', '<a>'], ['Which language is primairly usef for iOS app development', 'Swift', 'Kotlin', 'Swift'], ['What is the main purpose of CSS in web dev', 'To style and layout web pages', 'To structure the content of web pages', 'To style and layout web pages'], ['What does API stand for','Advanced Programming Instruction', 'Application Programming Interface', 'Application Programming Interface'],['In Java, what keyword is used to define a class', 'define', 'class', 'class'] ]
   return {
     question: "What is the capital of Germany?",
     choices: ["Berlin", "Paris"] as [string, string],
@@ -55,10 +56,10 @@ const PoseLandmarkerComponent: React.FC = () => {
       const newPoseLandmarker = await PoseLandmarker.createFromOptions(vision, {
         baseOptions: {
           modelAssetPath: `https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_heavy/float16/1/pose_landmarker_heavy.task`,
-          delegate: "GPU"
+          delegate: "GPU",
         },
         runningMode: "VIDEO",
-        numPoses: 3
+        numPoses: 3,
       });
       setPoseLandmarker(newPoseLandmarker);
     };
@@ -70,7 +71,6 @@ const PoseLandmarkerComponent: React.FC = () => {
       console.log("Wait! poseLandmarker not loaded yet.");
       return;
     }
-
     const video = videoRef.current;
     if (video) {
       if (webcamRunning) {
@@ -114,7 +114,7 @@ const PoseLandmarkerComponent: React.FC = () => {
             for (const landmark of result.landmarks) {
               const drawingUtils = new DrawingUtils(canvasCtx);
               drawingUtils.drawLandmarks(landmark, {
-                radius: (data: LandmarkData) => data.from ? DrawingUtils.lerp(data.from.z, -0.15, 0.1, 5, 1) : 1
+                radius: (data: LandmarkData) => (data.from ? DrawingUtils.lerp(data.from.z, -0.15, 0.1, 5, 1) : 1),
               });
               drawingUtils.drawConnectors(landmark, PoseLandmarker.POSE_CONNECTIONS);
             }
@@ -134,7 +134,6 @@ const PoseLandmarkerComponent: React.FC = () => {
             const leftAnkle = result.landmarks[0][28];
             const leftKnee = result.landmarks[0][26];
             const leftHip = result.landmarks[0][24];
-
             const rightAnkle = result.landmarks[0][27];
             const rightKnee = result.landmarks[0][25];
             const rightHip = result.landmarks[0][23];
@@ -143,67 +142,74 @@ const PoseLandmarkerComponent: React.FC = () => {
             let rightAngleS = calculateAngle(rightAnkle, rightKnee, rightHip);
             let leftAngleP = calculateAngle(leftShoulder, leftElbow, leftWrist);
             let rightAngleP = calculateAngle(rightShoulder, rightElbow, rightWrist);
-            if (leftAngleS > 80 && leftAngleS < 100 && rightAngleS > 80 && rightAngleS < 100){
+
+            console.log(`Left Arm Angle: ${leftAngleP}`);
+            console.log(`Right Arm Angle: ${rightAngleP}`);
+            console.log(`Left Leg Angle: ${leftAngleS}`);
+            console.log(`Right Leg Angle: ${rightAngleS}`);
+
+            const leftArmRaised = leftAngleP > 80 && leftAngleP < 100;
+            const rightArmRaised = rightAngleP > 80 && rightAngleP < 100;
+            const leftLegRaised = leftAngleS > 80 && leftAngleS < 100;
+            const rightLegRaised = rightAngleS > 80 && rightAngleS < 100;
+
+            if (leftArmRaised && rightArmRaised) {
               handleAnswer(question.choices[1] === question.correctAnswer);
-            }
-            else if (leftAngleP > 80 && leftAngleP < 100 && rightAngleP > 80 && rightAngleP < 100){
+            } else if (leftLegRaised && rightLegRaised) {
               handleAnswer(question.choices[0] === question.correctAnswer);
             }
           }
-        
-      });
-    }
-
+        });
+      }
       window.requestAnimationFrame(predictWebcam);
     }
   };
 
   return (
-    <section className='bg-hero-pattern'>
-    <div className="container ">
-      <div className="video-container">
-        <video ref={videoRef} style={{ display: 'block' }}></video>
-        <canvas ref={canvasRef} id="output_canvas" width="480" height="480"></canvas>
+    <section className="bg-hero-pattern">
+      <div className="container" onClick={enableCam}>
+        <div className="video-container">
+          <video ref={videoRef} style={{ display: 'block' }}></video>
+          <canvas ref={canvasRef} id="output_canvas" width="480" height="480"></canvas>
+        </div>
+        
+        <Flashcard
+          question={question.question}
+          choices={question.choices}
+          correctAnswer={question.correctAnswer}
+          onAnswer={handleAnswer}
+          onNewQuestion={handleNewQuestion}
+        />
+        
+        <Link href="/addFlashcard">
+          <button style={{ padding: '10px 10px', backgroundColor: 'blue' }}>Go to Home</button>
+        </Link>
+        <style>{`
+          .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+          }
+          .video-container {
+            position: relative;
+            width: 480px;
+            height: 480px;
+          }
+          video {
+            width: 100%;
+            height: 100%;
+          }
+          canvas {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+          }
+        `}</style>
       </div>
-      <button onClick={enableCam} id="webcamButton">
-        {webcamRunning ? "DISABLE PREDICTIONS" : "ENABLE PREDICTIONS"}
-      </button>
-      <Flashcard
-        question={question.question}
-        choices={question.choices}
-        correctAnswer={question.correctAnswer}
-        onAnswer={handleAnswer}
-        onNewQuestion={handleNewQuestion}
-      />
-      <Link href="/addFlashcard">
-        <button style={{padding: '10px 10px' }}>Go to Home</button>
-      </Link>
-      <style >{`
-        .container {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          justify-content: center;
-          height: 100vh;
-        }
-        .video-container {
-          position: relative;
-          width: 480px;
-          height: 480px;
-        }
-        video {
-          width: 100%;
-          height: 100%;
-        }
-        canvas {
-          position: absolute;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-        }
-      `}</style>
-    </div>
     </section>
   );
 };
